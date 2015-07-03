0c6c1fc810f8844387fb1c805d6c27293dc41110
Gaurav <gmi3024@gmail.com>
2015-07-04 00:14:36 +0530

    added search

--- binarytree.c
+++ binarytree.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "binarytree.h"
-#define SUCCESS 1
-#define FAILURE 0
 
 int main(){
 	struct Node *root = malloc(sizeof(struct Node));
@@ -20,81 +18,5 @@ int main(){
 	search(5, &root);
 	search(10, &root);
 	displayTree(&root);
-	return SUCCESS;
-}
-
-void displayTree(struct Node **root){
-	int i;
-	if((*root)==NULL){
-		return;
-	}
-	displayTree(&(*root)->left);
-	printf("%d\n", (*root)->data);
-	displayTree(&(*root)->right);
-}
-
-void search(int number, struct Node **root){
-	if((*root)==NULL){
-		printf("not found : %d\n", number);
-		return;
-	}
-	if(number == (*root)->data){
-		printf("found : %d\n", number);
-		return;
-	}else if(number <= (*root)->data){
-		search(number, &((*root)->left));
-	}else{
-		search(number, &((*root)->right));
-	}	
-}
-
-void traverseAndInsert(struct Node **node, struct Node **root){
-	if(*root==NULL){
-		*root = malloc(sizeof(struct Node));
-		(*root)->left = malloc(sizeof(struct Node));
-		(*root)->right = malloc(sizeof(struct Node));
-		(*root)->data = (*node)->data;
-		(*root)->right = NULL;
-		(*root)->left = NULL;
-		return;
-	}
-	if((*node)->data <= (*root)->data){
-		traverseAndInsert(node, &((*root)->left));
-	}else{
-		traverseAndInsert(node, &((*root)->right));
-	}
-}
-
-void deleteWithChildren(struct Node **nodeToBeDeleted){	
-	free(*nodeToBeDeleted);
-	*nodeToBeDeleted=NULL;
-}
-
-int deleteNode(int number, struct Node **root){
-	if(number == (*root)->data){
-		deleteWithChildren(root);
-		return SUCCESS;
-	}
-	if(number <= (*root)->data){
-		deleteNode(number, &((*root)->left));
-	}else{
-		deleteNode(number, &((*root)->right));
-	}
-	return FAILURE;
-}
-
-int insertNode(int number, struct Node **root){
-	struct Node *newNode = malloc(sizeof(struct Node));
-	newNode->data = number;
-	newNode->left = NULL;
-	newNode->right = NULL;
-	traverseAndInsert(&newNode, root);
-	return SUCCESS;
-}
-
-int initializeTree(int number, struct Node **root){	
-	(*root)->data = number;
-	(*root)->left = NULL;
-	(*root)->right = NULL;
 	return SUCCESS;
 }
--- binarytree.h
+++ binarytree.h
@@ -1,3 +1,6 @@
+#define SUCCESS 1
+#define FAILURE 0
+
 struct Node{
 	int data;	
 	struct Node *left;
@@ -12,3 +15,82 @@ int insertNode(int number, struct Node **tree);
 int deleteNode(int number, struct Node **root);
 void deleteWithChildren(struct Node **nodeToBeDeleted);
 void search(int number, struct Node **root);
+
+
+void displayTree(struct Node **root){
+	int i;
+	if((*root)==NULL){
+		return;
+	}
+	displayTree(&(*root)->left);
+	printf("%d\n", (*root)->data);
+	displayTree(&(*root)->right);
+}
+
+void search(int number, struct Node **root){
+	if((*root)==NULL){
+		printf("not found : %d\n", number);
+		return;
+	}
+	if(number == (*root)->data){
+		printf("found : %d\n", number);
+		return;
+	}else if(number <= (*root)->data){
+		search(number, &((*root)->left));
+	}else{
+		search(number, &((*root)->right));
+	}	
+}
+
+void traverseAndInsert(struct Node **node, struct Node **root){
+	if(*root==NULL){
+		*root = malloc(sizeof(struct Node));
+		(*root)->left = malloc(sizeof(struct Node));
+		(*root)->right = malloc(sizeof(struct Node));
+		(*root)->data = (*node)->data;
+		(*root)->right = NULL;
+		(*root)->left = NULL;
+		return;
+	}
+	if((*node)->data <= (*root)->data){
+		traverseAndInsert(node, &((*root)->left));
+	}else{
+		traverseAndInsert(node, &((*root)->right));
+	}
+}
+
+
+
+int deleteNode(int number, struct Node **root){
+	if(number == (*root)->data){
+		deleteWithChildren(root);
+		return SUCCESS;
+	}
+	if(number <= (*root)->data){
+		deleteNode(number, &((*root)->left));
+	}else{
+		deleteNode(number, &((*root)->right));
+	}
+	return FAILURE;
+}
+
+void deleteWithChildren(struct Node **nodeToBeDeleted){	
+	free(*nodeToBeDeleted);
+	*nodeToBeDeleted=NULL;
+}
+
+int insertNode(int number, struct Node **root){
+	struct Node *newNode = malloc(sizeof(struct Node));
+	newNode->data = number;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	traverseAndInsert(&newNode, root);
+	return SUCCESS;
+}
+
+int initializeTree(int number, struct Node **root){	
+	(*root)->data = number;
+	(*root)->left = NULL;
+	(*root)->right = NULL;
+	return SUCCESS;
+}
